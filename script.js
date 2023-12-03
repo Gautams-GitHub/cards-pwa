5929f13f9e4e7ea3b6caff51cce8fc780700ead8
gags <gagan.off.the.grid@gmail.com>
2023-12-04 01:16:22 +0530

    Applies Horizontal swipes and adds random image, solve the issue of skipping of last card

--- script.js
+++ script.js
@@ -3,8 +3,44 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentCardIndex = 0;
     let totalCards = 0;
     let touchstartY = 0;
+    let touchstartX = 0;
+    
+
+
+    // //To be commented out before pushing for deployment
+    // const cards= [
+    //     {
+    //       title: "Card 1",
+    //       imgSrc: "https://picsum.photos/400",
+    //       text: "This is the text for card 1."
+    //     },
+    //     {
+    //       title: "Card 2",
+    //       imgSrc: "https://picsum.photos/400",
+    //       text: "This is the text for card 2."
+    //     },
+    //     {
+    //       title: "Card 3",
+    //       imgSrc: "https://picsum.photos/400",
+    //       text: "This is the text for card 3."
+    //     },
+    //     {
+    //       title: "Card 4",
+    //       imgSrc: "https://picsum.photos/400",
+    //       text: "This is the text for card 4."
+    //     },
+    //     {
+    //       title: "Card 5",
+    //       imgSrc: "https://picsum.photos/400",
+    //       text: "This is the text for card 5."
+    //     }
+    //   ];    
+    //   /////
+
 
     function fetchAndDisplayData() {
+        //To be uncommented before pushing
+
         fetch('data.json')
             .then(response => response.json())
             .then(data => {
@@ -15,11 +51,23 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
+
+        //////////////
+
+
+
+        // //To be commented out before pushing for deployment
+        // //For testing without https
+        //         totalCards = cards.length;
+        //         // const dummyData = cards;
+        //         displayCard(cards[currentCardIndex]);
+        // /////////////////
     }
 
     function displayCard(cardData) {
         const cardContainer = document.getElementById('card-container');
-        if (currentCardIndex === totalCards - 1) {
+        cardContainer.innerHTML = '<div>Loading...<div/>';
+        if (currentCardIndex === totalCards ) {
             displayThankYouCard();
         } else {
             const cardHTML = generateCardHTML(cardData);
@@ -29,70 +77,78 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function displayThankYouCard() {
         const cardContainer = document.getElementById('card-container');
-        const thankYouHTML = generateCardHTML({
-            title: 'Thank You!',
-            imgSrc: './thankyou-image.png', // Replace with the actual path
-            text: 'We appreciate your time!'
-        });
-
+        const thankYouHTML = `
+        <div class="card">
+            <img src="./thankyou-image.png" alt="Thank You!">
+            <div class="title">Thank You!</div>
+            <div class="text">We appreciate your time!</div>
+        </div>
+        `;
         cardContainer.innerHTML = thankYouHTML;
     }
 
     function generateCardHTML(cardData) {
+        const randomNumber=600+Math.floor(Math.random() * 7);
         return `
             <div class="card">
-                <img src="${cardData.imgSrc}" alt="${cardData.title}">
+                <img src="${cardData.imgSrc}/${randomNumber}" alt="${cardData.title}">
                 <div class="title">${cardData.title}</div>
                 <div class="text">${cardData.text}</div>
             </div>
         `;
     }
 
-    function handleSwipe(deltaY) {
+    function handleSwipeVertical(deltaY) {
         if (deltaY > 50) {
             // Swipe up logic
             currentCardIndex = Math.max(currentCardIndex - 1, 0);
             fetchAndDisplayData();
         } else if (deltaY < -50) {
             // Swipe down logic
-            currentCardIndex = Math.min(currentCardIndex + 1, totalCards - 1);
+            currentCardIndex = Math.min(currentCardIndex + 1, totalCards);
             fetchAndDisplayData();
         }
     }
 
-    function handleScroll() {
-        if (isScrolledToBottom()) {
-            // Scroll down logic
-            currentCardIndex = Math.min(currentCardIndex + 1, totalCards - 1);
-            fetchAndDisplayData();
-        } else if (isScrolledToTop()) {
-            // Scroll up logic
+
+    function handleSwipeHorizontal(deltaX) {
+        if (deltaX > 50) {
+            // Swipe to the right logic
             currentCardIndex = Math.max(currentCardIndex - 1, 0);
             fetchAndDisplayData();
+        } else if (deltaX < -50) {
+            // Swipe to the left logic
+            
+            currentCardIndex = Math.min(currentCardIndex +1, totalCards );
+            fetchAndDisplayData();
         }
     }
 
-    function isScrolledToBottom() {
-        return window.innerHeight + window.scrollY >= document.body.offsetHeight;
-    }
-
-    function isScrolledToTop() {
-        return window.scrollY === 0;
-    }
+   
 
     function handleTouchStart(e) {
         touchstartY = e.touches[0].clientY;
+        touchstartX = e.touches[0].clientX;
+        
     }
 
-    function handleTouchEnd(e) {
-        const touchendY = e.changedTouches[0].clientY;
-        const deltaY = touchendY - touchstartY;
-        handleSwipe(deltaY);
+    function handleTouchEndVertical(e) {
+        const touchendY = e.changedTouches[0].clientY;        
+        const deltaY = touchendY - touchstartY;        
+        
+        handleSwipeVertical(deltaY);
     }
 
-    document.addEventListener('scroll', handleScroll);
+    function handleTouchEndHorizontal(e) {        
+        const touchendX = e.changedTouches[0].clientX;        
+        const deltaX = touchendX - touchstartX;
+        
+        handleSwipeHorizontal(deltaX);
+    }
+
+    
     document.addEventListener('touchstart', handleTouchStart);
-    document.addEventListener('touchend', handleTouchEnd);
+    document.addEventListener('touchend', handleTouchEndHorizontal);
 
     fetchAndDisplayData();
 });
